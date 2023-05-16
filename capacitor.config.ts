665c66904aaf71cba60e04a5a3018c98ba33e1f7
Lucas Lima <lucaslcsteam@hotmail.com>
2023-05-16 00:25:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-op-rpg',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
